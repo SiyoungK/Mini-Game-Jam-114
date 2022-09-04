1b65635a9ca136da08f24a16240e4a1c8650f9f4
Siyoung Kim <siyoungkim307@gmail.com>
2022-09-04 17:33:31 -0400

    Basic Enemy AI

---
+++ Mini Game Jam 114/Assets/Enemy.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health = 100;
+    private int hits = 0;
+    public int endurance = 3;
+
+
+    public void TakeDamage (int damage) {
+        health -= damage;
+
+        if (health <= 0) {
+            Die();
+        }
+
+        hits += 1;
+        if (hits == endurance) {
+            Freeze();
+            hits = 0;
+        }
+    }
+
+    public void Freeze () {
+        //Play freeze animation
+        //Pause movement
+    }
+
+    void Die() {
+        //Play death animation
+        Destroy(gameObject);
+    }
+}
