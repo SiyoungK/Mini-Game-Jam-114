b92631c294253dc6cfff1775318877358ca9b900
Ethan "Yeung Ethan Ethan Yeung Yeung Ethan" Yeung <yeungmethan@gmail.com>
2022-09-04 17:24:02 -0400

    Shooting, but no turning or aiming

--- Mini Game Jam 114/Assets/Scripts/PlayerMovement.cs
+++ Mini Game Jam 114/Assets/Scripts/PlayerMovement.cs
@@ -4,28 +4,31 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed;
+    public float moveSpeed = 5f;
     public Rigidbody2D rb;
-    private Vector2 moveDirection;
-    // Start is called before the first frame update
-    // Update is called once per frame
+    public Weapon weapon;
+
+    public Vector2 moveDirection;
+    public Vector2 mousePosition;
+
     void Update()
-    {
-        ProcessInputs();
-    }
-    void FixedUpdate()
-    {
-        Move();
-    }
-    void ProcessInputs()
     {
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
 
+        if (Input.GetMouseButtonDown(0))
+        {
+            weapon.Fire();
+        }
+
         moveDirection = new Vector2(moveX, moveY).normalized;
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
-    void Move()
+
+    private void FixedUpdate()
     {
         rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+        Vector2 aimDirection = mousePosition - rb.position;
+        float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f;
     }
 }
